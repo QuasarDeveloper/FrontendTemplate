197c2bde5520e645450b49dbc443fff8cba0d4f4
quasardeveloper <nitslaci@gmail.com>
2024-04-29 23:29:01 +0200

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+// import { useAppStore } from "../stores/appStore";
+// import { useOneStore } from "../stores/oneStore";
+// import { useManyStore } from "../stores/manyStore";
+// import { useOtherStore } from "src/stores/otherStore";
+// import { onMounted } from "vue";
+
+import { useRouter } from "vue-router";
+
+// const appStore = useAppStore();
+// const oneStore = useOneStore();
+// const manyStore = useManyStore();
+// const otherStore = useOtherStore();
+
+const router = useRouter();
+
+// onMounted(() => {
+//   storeMany.GetAll();
+// });
+
+function routerExample() {
+  router.push("/xempty");
+}
+</script>
+
+<template>
+  <q-page>
+    <div class="column flex-center" style="min-height: inherit">
+      <q-img width="150px" class="q-mb-xl" alt="Quasar logo" src="../assets/quasar-logo-vertical.svg" />
+      <q-btn color="teal" no-caps @click="$router.push('/xempty')">
+        <q-icon left size="3em" name="mdi-router" />
+        <div>Go Empty Page from template</div>
+      </q-btn>
+      <q-btn color="amber" class="q-mt-md" no-caps @click="routerExample">
+        <q-icon left size="3em" name="mdi-map" />
+        <div>Go Empty Page from script</div>
+      </q-btn>
+    </div>
+  </q-page>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+  font-size: 3vw;
+}
+</style>
---
+++ src/pages/xEmptyPage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// import { useRouter } from "vue-router";
+// import { useAppStore } from "../stores/appStore";
+// import { useOneStore } from "../stores/oneStore";
+// import { useManyStore } from "../stores/manyStore";
+// import { useOtherStore } from "../stores/otherStore";
+// import { onMounted } from "vue";
+
+// const appStore = useAppStore();
+// const oneStore = useOneStore();
+// const manyStore = useManyStore();
+// const otherStore = useOtherStore();
+
+// const router = useRouter();
+
+// onMounted(() => {
+//   manyStore.GetAll();
+//   router.push("/home");
+// });
+
+</script>
+
+<template>
+  <q-page></q-page>
+</template>
+
+<style lang="scss" scoped></style>
+
